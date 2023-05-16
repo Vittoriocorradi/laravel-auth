ab4e7807d9a19fe63e0910624454140518ba0b1d
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-16 16:21:15 +0200

    Project model, controller e migration

---
+++ database/migrations/2023_05_16_135528_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 75)->unique();
+            $table->string('status', 30);
+            $table->string('type', 40);
+            $table->date('starting_date');
+            $table->date('finish_date')->nullable();
+            $table->text('overview');
+            $table->string('objectives');
+            $table->text('roadmap')->nullable();
+            $table->string('priority', 30);
+            $table->text('contributors')->nullable();
+            $table->tinyInteger('is_finished')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
