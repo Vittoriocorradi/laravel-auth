971376e2fafd95bbcfd96988b3913d93af23347f
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-19 17:39:07 +0200

    feat: alert fadeout file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,8 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import './myfunctions/alert-fadeout';       //Function to make the alert message in index fade out
 import.meta.glob([
     '../img/**'
 ])
 
-const alertMessage = document.getElementById('alert-message');
-
-alertMessage.style.opacity = 1;
-
-setTimeout(function() {
-    
-    alertMessage.style.opacity = 0;
-},2000);
---
+++ resources/js/myfunctions/alert-fadeout.js
@@ -0,0 +1,10 @@
+// Function to make the alert for successful creation/editing/deleting of a project fade out after two seconds
+
+const alertMessage = document.getElementById('alert-message');
+
+alertMessage.style.opacity = 1;
+
+setTimeout(function() {
+    
+    alertMessage.style.opacity = 0;
+},2000);
