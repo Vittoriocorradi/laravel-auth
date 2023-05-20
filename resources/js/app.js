73ea2091c0eb73f1c7cdbf83504e22763a041ae0
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-20 22:06:11 +0200

    feat: preview image file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,9 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
-import './myfunctions/alert-fadeout';       //Function to make the alert message in index fade out
+import './myfunctions/alert-fadeout'; 
+import './myfunctions/image-preview';      //Function to make the alert message in index fade out
 import.meta.glob([
     '../img/**'
 ])
 
-const imageInput = document.querySelector('#image');
-
-imageInput.addEventListener('change', showPreview);
-
-function showPreview(event) {
-    if (event.target.files.length > 0) {
-        const src = URL.createObjectURL(event.target.files[0]);
-        const preview = document.getElementById("file-image-preview");
-        preview.src = src;
-        preview.style.display = "block";
-    }
-}
--- resources/js/myfunctions/alert-fadeout.js
+++ resources/js/myfunctions/alert-fadeout.js
@@ -1,10 +1,13 @@
-// // Function to make the alert for successful creation/editing/deleting of a project fade out after two seconds
+// Function to make the alert for successful creation/editing/deleting of a project fade out after two seconds
 
-// const alertMessage = document.getElementById('alert-message');
+if (document.getElementById('alert-message')) {
 
-// alertMessage.style.opacity = 1;
-
-// setTimeout(function() {
+    const alertMessage = document.getElementById('alert-message');
     
-//     alertMessage.style.opacity = 0;
-// },2000);
+    alertMessage.style.opacity = 1;
+    
+    setTimeout(function() {
+        
+        alertMessage.style.opacity = 0;
+    },2000);
+}
